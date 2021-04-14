1cd6f7e6c7d9af1d4d096b970830e3896871390d
Irenia111 <15000616693@163.com>
2021-04-14 16:08:21 +0800

    init the prototype classes

---
+++ src/main/java/com/irenia/blog/prototype/Type.java
@@ -0,0 +1,25 @@
+package com.irenia.blog.prototype;
+
+public class Type {
+    private Long id;
+    private String name;
+
+    public Type() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
